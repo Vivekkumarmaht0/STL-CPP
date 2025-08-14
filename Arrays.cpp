3a76abc239144bb610e1081240a97531568e85ec
Vivek Kumar Mahto <vivekkumarmahto101103@gmail.com>
2025-08-14 17:28:14 +0530

    Arrays

--- Arrays.cpp
+++ Arrays.cpp
@@ -5,12 +5,18 @@ int main(){
     // Basic Array
     int basic[3] = {1, 2, 3};
 
+    for(int i: basic){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
     array<int,4> A = {1, 2, 3, 4};
     int size = A.size();  // Size
 
     for(int i=0; i<size; i++){
-        cout<<A[i]<<endl;
+        cout<<A[i]<<" ";
     }
+    cout<<endl;
 
     cout<< "Element at 2nd Index: " << A.at(2) << endl;
     cout<< "Element empty or not: " << A.empty() << endl;
